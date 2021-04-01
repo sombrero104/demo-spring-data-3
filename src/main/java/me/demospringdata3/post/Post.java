9e2a85580de15384138579d92bc99f45ced961de
sombrero104 <sombrero.khy@gmail.com>
2021-04-01 21:52:46 +0900

    .

--- src/main/java/me/demospringdata3/post/Post.java
+++ src/main/java/me/demospringdata3/post/Post.java
@@ -16,4 +16,28 @@ public class Post {
     @Temporal(TemporalType.TIMESTAMP)
     private Date created;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
 }
---
+++ src/main/java/me/demospringdata3/post/PostController.java
@@ -0,0 +1,29 @@
+package me.demospringdata3.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class PostController {
+
+    private final PostRepository postRepository;
+
+    /**
+     * 생성자가 하나만 있고, PostRepository가 빈으로 등록되어 있는 경우에는 자동으로 빈을 주입해 준다.
+     */
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @GetMapping("/posts/{id}") // 원래 id는 문자열로 받지만
+    public Post getPost(@PathVariable Long id) { // Long형으로 데이터바인더가 기본적으로 바인딩해준다.
+        Optional<Post> byId = postRepository.findById(id);
+        Post post = byId.get();
+        return post;
+    }
+
+}
