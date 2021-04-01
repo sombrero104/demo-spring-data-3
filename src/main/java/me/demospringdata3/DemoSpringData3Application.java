adb12b692d3b53f4380b0639277ce408e8484be8
sombrero104 <sombrero.khy@gmail.com>
2021-04-01 21:39:42 +0900

    .

---
+++ src/main/java/me/demospringdata3/DemoSpringData3Application.java
@@ -0,0 +1,13 @@
+package me.demospringdata3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringData3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringData3Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/demospringdata3/DemoSpringData3ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.demospringdata3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringData3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
