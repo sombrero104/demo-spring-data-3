89489260e370d8fe6f2e668e9a012ac2b0df1543
sombrero104 <sombrero.khy@gmail.com>
2021-04-02 21:26:19 +0900

    .

--- src/main/java/me/demospringdata3/post/PostRepository.java
+++ src/main/java/me/demospringdata3/post/PostRepository.java
@@ -10,6 +10,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByTitleStartsWith(String title);
 
     @Query("SELECT p FROM Post AS p WHERE p.title = ?1")
+    // @Query(value = "", nativeQuery = true) // native query를 쓴 경우에는 nativeQuery = true 옵션을 줘서 알려준다.
     List<Post> findByTitle(String title);
 
 }
