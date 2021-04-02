7a68d157f6e3453aedabdd19160acd7e7707f4e6
sombrero104 <sombrero.khy@gmail.com>
2021-04-02 21:14:31 +0900

    .

--- src/main/java/me/demospringdata3/post/PostRepository.java
+++ src/main/java/me/demospringdata3/post/PostRepository.java
@@ -2,5 +2,10 @@ package me.demospringdata3.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByTitleStartsWith(String title);
+
 }
