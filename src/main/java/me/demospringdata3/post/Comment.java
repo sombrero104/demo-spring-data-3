fb137c49445d7617edd6599265e50de90592e548
sombrero104 <sombrero.khy@gmail.com>
2021-04-02 23:51:15 +0900

    .

--- src/main/java/me/demospringdata3/post/Comment.java
+++ src/main/java/me/demospringdata3/post/Comment.java
@@ -14,4 +14,28 @@ public class Comment {
     @ManyToOne(fetch = FetchType.LAZY)
     private Post post;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
 }
--- src/test/java/me/demospringdata3/post/CommentRepositoryTest.java
+++ src/test/java/me/demospringdata3/post/CommentRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -12,9 +14,12 @@ class CommentRepositoryTest {
     @Autowired
     CommentRepository commentRepository;
 
+    @Autowired
+    PostRepository postRepository;
+
     @Test
     void getComment() {
-        commentRepository.findById(1l);
+        // commentRepository.findById(1l);
         /**
          * @ManyToOne에서는 Fetch 모드를 설정하지 않으면 기본값이 EAGER이기 때문에
          * comment 하나를 가져올 때 연관된 post 데이터도 같이 가져온다.
@@ -34,9 +39,12 @@ class CommentRepositoryTest {
          *     where
          *         comment0_.id=?
          */
+
+        // commentRepository.findById(1l);
         /**
          * Comment에서 Post의 Fetch 모드를 Lazy로 바꾸면 comment만 가져온다.
          * 만약 Comment의 Post에 접근하게 되면 그때서야 Post도 같이 조회하는 쿼리가 날아간다.
+         * (Comment 인스턴스가 Persistent 상태(Persistent context가 관리하는 상태)이어야지만 가능하다.)
          * select
          *         comment0_.id as id1_0_0_,
          *         comment0_.comment as comment2_0_0_,
