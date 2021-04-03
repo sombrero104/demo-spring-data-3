3e891b57a1b1de57ea048c22eb8cac1501ae43d1
sombrero104 <sombrero.khy@gmail.com>
2021-04-03 20:59:22 +0900

    .

--- src/main/java/me/demospringdata3/post/CommentSpecs.java
+++ src/main/java/me/demospringdata3/post/CommentSpecs.java
@@ -10,24 +10,30 @@ import javax.persistence.criteria.Root;
 public class CommentSpecs {
 
     public static Specification<Comment> isBest() {
-        return new Specification<Comment>() {
+        /*return new Specification<Comment>() {
             @Override
             public Predicate toPredicate(Root<Comment> root,
                                          CriteriaQuery<?> query,
                                          CriteriaBuilder builder) {
                 return builder.isTrue(root.get(Comment_.best)); // Comment의 best 값이 true인지.
             }
+        };*/
+        return (Specification<Comment>) (root, query, builder) -> { // 람다식으로 표현.
+            return builder.isTrue(root.get(Comment_.best)); // Comment의 best 값이 true인지.
         };
     }
 
     public static Specification<Comment> isGood() {
-        return new Specification<Comment>() {
+        /*return new Specification<Comment>() {
             @Override
             public Predicate toPredicate(Root<Comment> root,
                                          CriteriaQuery<?> query,
                                          CriteriaBuilder builder) {
                 return builder.greaterThanOrEqualTo(root.get(Comment_.up), 10); // Comment의 up 값이 10 이상인지.
             }
+        };*/
+        return (Specification<Comment>) (root, query, builder) -> { // 람다식으로 표현.
+            return builder.greaterThanOrEqualTo(root.get(Comment_.up), 10); // Comment의 up 값이 10 이상인지.
         };
     }
 
